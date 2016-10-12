f299bdd2791d88734deaa91fbd5fd6205987c20c
Yullix <tedted93@hotmail.com>
2016-10-12 18:53:29 +0200

    Init. Added Enum to represent Pieces. Added Enum to represent Squares.

---
+++ Chess_Database/Enums/Piece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Database.Enums
+{
+    public enum Piece
+    {
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King
+    }
+}
