fb9194273328942caf4483b252831a606c37d781
Yullix <tedted93@hotmail.com>
2016-10-12 18:59:07 +0200

    Init Added Enum for Pieces. Added Enum for Squares.

--- Chess_Database/Enums/Piece.cs
+++ Chess_Database/Enums/Piece.cs
@@ -8,11 +8,11 @@ namespace Chess_Database.Enums
 {
     public enum Piece
     {
-        Pawn,
-        Knight,
-        Bishop,
-        Rook,
-        Queen,
-        King
+        Pawn = 1,
+        Knight = 2,
+        Bishop = 3,
+        Rook = 4,
+        Queen = 5,
+        King = 6
     }
 }
